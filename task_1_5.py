050791fb3c0b00d4466aa5835467571f72f6e656
Dkfore <82409498+Dkfore@users.noreply.github.com>
2021-04-13 21:36:35 +0300

    Home tasks
    no any changes

---
+++ task_1_5.py
@@ -0,0 +1,10 @@
+# Даны катеты прямоугольного треугольника. Найти его гипотенузу и площадь.
+
+x = 3
+y = 4
+
+hypotenuse = (x**2 + y**2) ** 0.5
+print("hypotenuse = ", hypotenuse)
+
+square = (x * y) / 2
+print("square = ", square)
